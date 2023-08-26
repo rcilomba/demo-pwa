40c56f00995274a917591c8be6ef384cb2759d3e
Ramadan Cilomba <rcilomba@yahoo.com>
2023-08-26 22:28:31 +0200

    restart app pwa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Outlet } from 'react-router-dom';
-import PWAPrompt from 'react-ios-pwa-prompt';
 
 function App() {
   return (
-      <>
-            <header>
-                <p>This is header</p>
-            </header>
-            <main>
-                <Outlet />
-                <footer>
-                    <p>Do it properly</p>
-                </footer>
-                <PWAPrompt copyTitle="Føj til hjemmeskærmen"/>
-            </main>
-        </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
