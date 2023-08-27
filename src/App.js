277126d8d6a2377870dad8c81fcc658c6f5e9f1d
Ramadan Cilomba <rcilomba@yahoo.com>
2023-08-27 19:44:37 +0200

    add watchpos and navigation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import PWAPrompt from 'react-ios-pwa-prompt';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
       <>
         <header>
             <p>This is header</p>
+            <nav>
+            <Link to="/">Home</Link> &nbsp;&nbsp;
+            <Link to="/location">Location</Link> &nbsp;&nbsp;
+
+            </nav>
         </header>
         <main>
             <Outlet />
--- src/pages/Location.js
+++ src/pages/Location.js
@@ -3,27 +3,46 @@ import { useState, useEffect } from 'react';
 const Location = () => {
 
     const [myPos, setMyPos] = useState();
+    const [dynPos, setDynPos] = useState();
 
     useEffect(() => {
         if('geolocation' in navigator){
-            navigator.geolocation.watchPosition((position) => { // from getCurrentPosition to watchPosition
+            navigator.geolocation.getCurrentPosition((position) => { // from getCurrentPosition to watchPosition
                 console.log(position)
                 setMyPos(position)
             })
         }
     }, []);
+
+    const handleDynamicPosition = (position) => {
+            setDynPos(position)
+    }
+
+    const handleDynamicPositionError = (err) => {
+        console.log(err)
+    }
+
+    navigator.geolocation.watchPosition(handleDynamicPosition, handleDynamicPositionError, {timeout: 5000})
         return ( 
         <>
-            <h1>This is Location!</h1>
+            <h1>This is static Location!</h1>
   
             <p>Latitude: {myPos?.coords.latitude}</p>
             <p>Longitude: {myPos?.coords.longitude}</p>
             <p>Altitude: {myPos?.coords.altitude}</p>
             <p>Speed: {myPos?.coords.speed}</p>
             <p>Accuracy: {myPos?.coords.accuracy}</p>
-        
-        </> 
-    );
+
+            <h1>This is dynamic position</h1>
+            <>
+            <p>Latitude: {dynPos?.coords.latitude}</p>
+            <p>Longitude: {dynPos?.coords.longitude}</p>
+            <p>Altitude: {dynPos?.coords.altitude}</p>
+            <p>Speed: {dynPos?.coords.speed}</p>
+            <p>Accuracy: {dynPos?.coords.accuracy}</p>
+            </>
+        </>
+     );
 }
 
 export default Location;
